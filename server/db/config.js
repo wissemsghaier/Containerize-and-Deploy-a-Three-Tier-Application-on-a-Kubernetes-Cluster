f7746b69f7c63564f9e1c90f00d8e0bd061e44b1
wissem.sghaier <wissem.sghaier@artificial-insight.co>
2024-11-19 19:13:28 +0100

    first commit

--- server/db/config.js
+++ server/db/config.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/e-com",{
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    family: 4,
+ const mongoose = require('mongoose');
+ mongoose.connect("mongodb://mongo:27017/e-com",{
+     useNewUrlParser: true, 
+     useUnifiedTopology: true,
+     family: 4,
 });
 //mongoose.connect('mongodb://127.0.0.1/test')
+
+// const mongoose = require('mongoose');
+// const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017/e-com";
+// mongoose.connect(mongoUri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     family: 4,
+// });
